bcf2e3ed77b0b8b5abe0e85b07c1b24338f8d69f
Aldo-CO <aldoyael.onion@gmail.com>
2023-10-25 00:05:33 -0600

    Clase de Controllers

--- routes/web.php
+++ routes/web.php
@@ -1,24 +1,19 @@
 <?php
 
+use App\Http\Controllers\PageController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('home');
-})->name('home');
+/* Route::get('/', [PageController::class, 'home'])->name('home');
 
-Route::get('blog', function () {
+Route::get('blog', [PageController::class, 'blog'])->name('blog');
 
-    $posts = [
-        ['id' => 1, 'title' => 'PHP', 'slug' => 'php'],
-        ['id' => 2, 'title' => 'Laravel', 'slug' => 'laravel'],
-    ];
+Route::get('blog/{slug}', [PageController::class, 'post'])->name('post'); */
 
-    return view('blog', ['posts' => $posts]);
-})->name('blog');
+Route::controller(PageController::class)->group(function ()
+{
+    Route::get('/', 'home')->name('home');
 
-Route::get('blog/{slug}', function ($slug) {
+    Route::get('blog', 'blog')->name('blog');
 
-    $post = $slug;
-
-    return view('post', ['post' => $post]);
-})->name('post');
+    Route::get('blog/{slug}', 'post')->name('post');
+});
